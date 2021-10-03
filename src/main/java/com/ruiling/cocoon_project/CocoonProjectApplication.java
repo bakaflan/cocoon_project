39549d68f90b147fc55048c6c05b42ff5d864a8d
Ruiling Guo <ruiling.guo@thoughtworks.com>
2021-10-03 19:26:58 +0800

    [ruiling.guo][TECH] - init project

---
+++ src/main/java/com/ruiling/cocoon_project/CocoonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ruiling.cocoon_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruiling/cocoon_project/CocoonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruiling.cocoon_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
